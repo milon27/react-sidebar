e82db39438cdf9cd90e19ddd958d8f12fe948727
milon27 <mdjahidulislammilon@gmail.com>
2022-03-29 21:59:47 +0600

    fix window

--- src/sidebar/createNextLink.tsx
+++ src/sidebar/createNextLink.tsx
@@ -1,15 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { SideBarContext } from './SidebarWrapper';
 
 export const createNextNavLink = (MainNavLink: any, title: string, to: string, icon: React.ReactNode) => {
     return () => {
         const { small } = useContext(SideBarContext);
-
-        const isActive = typeof window !== "undefined" && window.location.pathname === to
+        const [active, setActive] = useState(false)
+        useEffect(() => {
+            const isActive = typeof window !== "undefined" && window.location.pathname === to
+            setActive(isActive)
+        }, [active])
 
         return <>
             <MainNavLink href={to}>
-                <a className={`my-2 p-2 rounded hover:bg-slate-200 flex gap-2 items-center ${isActive === true ? "bg-slate-200" : ""}`}
+                <a className={`my-2 p-2 rounded hover:bg-slate-200 flex gap-2 items-center ${active === true ? "bg-slate-200" : ""}`}
                 >
                     {icon}<span className={`${small === true ? "hidden" : "block"} `}>{title}</span>
                 </a>
